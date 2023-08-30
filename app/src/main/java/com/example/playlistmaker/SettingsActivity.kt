d412de70e507545ef45b14eea7e7d236f77cc8ae
stoutgeneral <gnganggang@gmail.com>
2023-08-31 04:01:03 +0700

    Спринт 8\23.

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -2,19 +2,19 @@ package com.example.playlistmaker
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
-class SettingsActivity: AppCompatActivity() {
+class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val arrowBack =  findViewById<ImageView>(R.id.arrow_back)
-
+        val arrowBack = findViewById<ImageView>(R.id.arrow_back)
         arrowBack.setOnClickListener {
-            val arrowBackIntent = Intent (this, MainActivity::class.java)
-            startActivity(arrowBackIntent)
+            finish()
         }
     }
 }

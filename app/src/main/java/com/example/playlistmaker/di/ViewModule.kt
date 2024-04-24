89973b0a2270e28d6017ac52a6a04af87baf5c57
stoutgeneral <erin1997@yandex.ru>
2024-04-25 04:02:32 +0700

    Спринт 19\25

--- app/src/main/java/com/example/playlistmaker/di/ViewModule.kt
+++ app/src/main/java/com/example/playlistmaker/di/ViewModule.kt
@@ -3,7 +3,7 @@ package com.example.playlistmaker.di
 import com.example.playlistmaker.presentation.audioplayer.AudioPlayerViewModel
 import com.example.playlistmaker.presentation.library.FavoritesFragmentViewModel
 import com.example.playlistmaker.presentation.library.FragmentLibraryPlaylistViewModel
-import com.example.playlistmaker.presentation.main.MainViewModel
+import com.example.playlistmaker.presentation.root.RootViewModel
 import com.example.playlistmaker.presentation.search.SearchViewModel
 import com.example.playlistmaker.presentation.settings.SettingsViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -19,7 +19,7 @@ val viewModule = module {
     }
 
     viewModel {
-        MainViewModel(get())
+        RootViewModel(get())
     }
 
     viewModel {
---
+++ app/src/main/java/com/example/playlistmaker/ui/library/LibraryFragment.kt
@@ -0,0 +1,48 @@
+package com.example.playlistmaker.ui.library
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayoutMediator
+import com.example.playlistmaker.R
+import com.example.playlistmaker.databinding.FragmentLibraryBinding
+
+class LibraryFragment : Fragment() {
+
+    private lateinit var tabLayoutMediator: TabLayoutMediator
+    private lateinit var binding: FragmentLibraryBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentLibraryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.viewPager.adapter = LibraryViewPagerAdapter(childFragmentManager, lifecycle)
+
+        tabLayoutMediator =
+            TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+                when (position) {
+                    0 -> tab.text = getString(R.string.favorites_track)
+                    1 -> tab.text = getString(R.string.library_playlist)
+                }
+            }
+        tabLayoutMediator.attach()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        tabLayoutMediator.detach()
+    }
+
+    }
+
+
+
+
